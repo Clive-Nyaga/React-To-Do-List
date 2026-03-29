8e610e45cc5761b778f4aaf1eb40329bbfdce8e6
Clive Nyaga <nyagakagotho@gmail.com>
2026-03-29 19:41:46 +0300

    Adds edit and delete buttons to task cards

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
     let todos = [
@@ -11,7 +12,9 @@ export default function TodoList() {
     <ul className='main'>
         {todos.map((todo, todoIndex) => {
             return (
-                <li className='todoItem' key={todoIndex}>{todo}</li>
+                <TodoCard key={todoIndex}>
+                    <p>{todo}</p>
+                </TodoCard>
             )
         })}
     </ul>
